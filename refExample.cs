6a951dc47912e5a3943d5485cb221b2678fc1305
unknown <kdhz2012@gmail.com>
2023-01-19 09:46:36 +0900

    230118

---
+++ refExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+//namespace
+
+public class Point
+{
+    Point()
+    {
+
+    }
+}
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        //Point pt1 = new Point();
+
+        //Change(ref pt1);
+        //Console.WriteLine("pt1 : X -> " + pt1.x + " y ")
+    }
+}
+
+//private static void Change(ref Point pt)
+//{
+
+//}
+
